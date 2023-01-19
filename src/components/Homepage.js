519441d0d91c0d0e8d0ccd299a203cdcb99411d4
Shoshana Levitt <shoshana.levitt@gmail.com>
2023-01-19 14:14:09 -0500

    removed homepage text, made intro page component

--- src/components/Homepage.js
+++ src/components/Homepage.js
@@ -1,19 +1,13 @@
 import React from "react";
 import { HashLink } from "react-router-hash-link";
 import { Typography, Box } from "@mui/material";
+import Intro from "./Intro";
 import AboutMe from "./AboutMe";
 
 const Homepage = () => {
   return (
     <div>
-      <Box height="100vh" width="100vw" display="flex">
-        <Typography variant="h4" align="center" margin={10}>
-          Welcome to Shoshana's Website!
-        </Typography>
-        <Typography variant="h6" align="center">
-          Please pardon our appearance while we're under construction.
-        </Typography>
-      </Box>
+      <Intro />
       <AboutMe />
     </div>
   );
--- src/components/Main.js
+++ src/components/Main.js
@@ -3,7 +3,8 @@ import { Link } from "react-scroll";
 import Homepage from "./Homepage";
 import { Route, Routes, BrowserRouter } from "react-router-dom";
 import { HashLink } from "react-router-hash-link";
-import { Button, AppBar, Toolbar } from "@mui/material";
+import { Button, AppBar, Toolbar, IconButton } from "@mui/material";
+import { Home } from "@mui/icons-material";
 /*</Box>
     This is you entry point for your routes
 */
@@ -13,8 +14,18 @@ const Main = () => {
       <div>
         <AppBar position="fixed">
           <Toolbar sx={{ p: -1 }}>
-            <Button href="/" color="secondary" variant="text" sx={{ m: 1 }}>
-              Home
+            <IconButton href="/">
+              <Home></Home>
+            </IconButton>
+            <Button
+              component={HashLink}
+              smooth
+              to="#intro"
+              color="secondary"
+              variant="text"
+              sx={{ m: 1 }}
+            >
+              Intro
             </Button>
             <Button
               component={HashLink}
