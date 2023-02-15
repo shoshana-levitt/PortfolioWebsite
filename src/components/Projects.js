4166f7da9f41c936781c1a3ba85d8d364bda5b06
Shoshana Levitt <shoshana.levitt@gmail.com>
2023-02-15 15:59:10 -0500

    started to add augmented and gc to project cards

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -69,17 +69,19 @@ const Projects = () => {
                     />
                     <CardContent>
                       <Typography gutterBottom variant="h5" component="div">
-                        Project #1
+                        augmentEd♫⁺
                       </Typography>
                       <Typography variant="body2" color="text.secondary">
-                        Cupcake ipsum dolor sit amet. Wafer apple pie cake
-                        halvah tiramisu. Marzipan marshmallow pie muffin sweet
-                        roll fruitcake donut.
+                        Peer-to-peer music education software designed for
+                        people who want to learn and teach music. Users can
+                        craft lessons using an editable musical staff, view the
+                        lessons created by other users, and play a virtual piano
+                        using their computer's keyboard.
                       </Typography>
                     </CardContent>
                     <CardActions>
                       <IconButton
-                        href="https://github.com/shoshana-levitt"
+                        href="https://github.com/GHP-Team-C/Capstone"
                         target="_blank"
                         disableRipple
                         sx={{ m: 1, ":hover": { color: "#2085a0" } }}
@@ -87,7 +89,7 @@ const Projects = () => {
                         <GitHub></GitHub>
                       </IconButton>
                       <IconButton
-                        href="https://github.com/shoshana-levitt"
+                        href="https://augmented.onrender.com/"
                         target="_blank"
                         disableRipple
                         sx={{ m: 1, ":hover": { color: "#2085a0" } }}
@@ -112,7 +114,7 @@ const Projects = () => {
                     />
                     <CardContent>
                       <Typography gutterBottom variant="h5" component="div">
-                        Project #2
+                        Grace Shopper
                       </Typography>
                       <Typography variant="body2" color="text.secondary">
                         Cupcake ipsum dolor sit amet. Wafer apple pie cake
@@ -122,64 +124,21 @@ const Projects = () => {
                     </CardContent>
                     <CardActions>
                       <IconButton
-                        href="https://github.com/shoshana-levitt"
+                        href="https://github.com/shoshana-levitt/EcommerceSite"
                         target="_blank"
                         disableRipple
                         sx={{ m: 1, ":hover": { color: "#2085a0" } }}
                       >
                         <GitHub></GitHub>
                       </IconButton>
-                      <IconButton
+                      {/* <IconButton
                         href="https://github.com/shoshana-levitt"
                         target="_blank"
                         disableRipple
                         sx={{ m: 1, ":hover": { color: "#2085a0" } }}
                       >
                         <Launch></Launch>
-                      </IconButton>
-                    </CardActions>
-                  </Card>
-                </motion.div>
-                <motion.div
-                  whileHover={{ scale: 1.05 }}
-                  transition={{ type: "spring", stiffness: 400, damping: 10 }}
-                >
-                  <Card
-                    sx={{ width: 375 }}
-                    style={{ backgroundColor: "#dfdfdf" }}
-                  >
-                    <CardMedia
-                      sx={{ height: 200 }}
-                      component="img"
-                      image="https://picsum.photos/400/300"
-                    />
-                    <CardContent>
-                      <Typography gutterBottom variant="h5" component="div">
-                        Project #3
-                      </Typography>
-                      <Typography variant="body2" color="text.secondary">
-                        Cupcake ipsum dolor sit amet. Wafer apple pie cake
-                        halvah tiramisu. Marzipan marshmallow pie muffin sweet
-                        roll fruitcake donut.
-                      </Typography>
-                    </CardContent>
-                    <CardActions>
-                      <IconButton
-                        href="https://github.com/shoshana-levitt"
-                        target="_blank"
-                        disableRipple
-                        sx={{ m: 1, ":hover": { color: "#2085a0" } }}
-                      >
-                        <GitHub></GitHub>
-                      </IconButton>
-                      <IconButton
-                        href="https://github.com/shoshana-levitt"
-                        target="_blank"
-                        disableRipple
-                        sx={{ m: 1, ":hover": { color: "#2085a0" } }}
-                      >
-                        <Launch></Launch>
-                      </IconButton>
+                      </IconButton> */}
                     </CardActions>
                   </Card>
                 </motion.div>
