f04ee7eab8503c340896f1f6bc7af2005c57a9a2
shoshana.levitt@gmail.com <shoshana.levitt@gmail.com>
2023-04-05 08:45:03 -0700

    updated resume link

--- src/components/Main.js
+++ src/components/Main.js
@@ -135,7 +135,7 @@ const Main = () => {
             </IconButton>
             <Button
               startIcon={<Description />}
-              href="https://acrobat.adobe.com/link/track?uri=urn:aaid:scds:US:8ea3e6ce-2ffc-3f31-a872-dfa9d6aac50e"
+              href="https://acrobat.adobe.com/link/track?uri=urn:aaid:scds:US:286b3ffe-3d8c-39cb-9870-935538780884"
               target="_blank"
               color="primary"
               variant="outlined"
