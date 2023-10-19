ca107e55708121826e138e55b7213322c3dacbbf
shoshana.levitt@gmail.com <shoshana.levitt@gmail.com>
2023-10-18 20:26:43 -0400

    add transition to AboutMe Timeline cards

--- src/components/Timeline.js
+++ src/components/Timeline.js
@@ -11,6 +11,7 @@ import {
 import TimelineOppositeContent, {
   timelineOppositeContentClasses,
 } from "@mui/lab/TimelineOppositeContent";
+import { motion } from "framer-motion";
 
 const TimelineComponent = () => {
   return (
@@ -35,17 +36,23 @@ const TimelineComponent = () => {
           <TimelineConnector />
         </TimelineSeparator>
         <TimelineContent>
-          <Card style={{ backgroundColor: "rgba(254, 253, 253, 0.1)" }}>
-            <CardContent>
-              <Typography variant="subtitle2" color="primary">
-                My academic journey at NYU provided a robust foundation for my
-                career in technology. My degree from the Steinhardt School
-                equipped me with the essential tools to dissect the
-                sociological, political, and cultural dimensions of our media
-                landscape.
-              </Typography>
-            </CardContent>
-          </Card>
+          <motion.div
+            whileHover={{ scale: 1.01 }}
+            transition={{ type: "spring", stiffness: 400, damping: 10 }}
+            key={1}
+          >
+            <Card style={{ backgroundColor: "rgba(254, 253, 253, 0.1)" }}>
+              <CardContent>
+                <Typography variant="subtitle2" color="primary">
+                  My academic journey at NYU provided a robust foundation for my
+                  career in technology. My degree from the Steinhardt School
+                  equipped me with the essential tools to dissect the
+                  sociological, political, and cultural dimensions of our media
+                  landscape.
+                </Typography>
+              </CardContent>
+            </Card>
+          </motion.div>
         </TimelineContent>
       </TimelineItem>
       <TimelineItem>
@@ -70,18 +77,24 @@ const TimelineComponent = () => {
           <TimelineConnector />
         </TimelineSeparator>
         <TimelineContent>
-          <Card style={{ backgroundColor: "rgba(254, 253, 253, 0.1)" }}>
-            <CardContent>
-              <Typography variant="subtitle2" color="primary">
-                In my client-facing roles, for both B2B and B2C products, I
-                developed a unique perspective on how products can be improved
-                to better serve the needs of the end user. I honed my ability to
-                collaborate with cross-functional teams and communicate
-                effectively with both engineering and non-engineering
-                stakeholders.
-              </Typography>
-            </CardContent>
-          </Card>
+          <motion.div
+            whileHover={{ scale: 1.01 }}
+            transition={{ type: "spring", stiffness: 400, damping: 10 }}
+            key={2}
+          >
+            <Card style={{ backgroundColor: "rgba(254, 253, 253, 0.1)" }}>
+              <CardContent>
+                <Typography variant="subtitle2" color="primary">
+                  In my client-facing roles, for both B2B and B2C products, I
+                  developed a unique perspective on how products can be improved
+                  to better serve the needs of the end user. I honed my ability
+                  to collaborate with cross-functional teams and communicate
+                  effectively with both engineering and non-engineering
+                  stakeholders.
+                </Typography>
+              </CardContent>
+            </Card>
+          </motion.div>
         </TimelineContent>
       </TimelineItem>
       <TimelineItem>
@@ -98,16 +111,22 @@ const TimelineComponent = () => {
           <TimelineDot />
         </TimelineSeparator>
         <TimelineContent>
-          <Card style={{ backgroundColor: "rgba(254, 253, 253, 0.1)" }}>
-            <CardContent>
-              <Typography variant="subtitle2" color="primary">
-                Through my training in an accelerated bootcamp, I gained
-                proficiency in Javascript and the PERN stack. I developed
-                applications both solo and in teams, using Git and agile project
-                management.
-              </Typography>
-            </CardContent>
-          </Card>
+          <motion.div
+            whileHover={{ scale: 1.01 }}
+            transition={{ type: "spring", stiffness: 400, damping: 10 }}
+            key={3}
+          >
+            <Card style={{ backgroundColor: "rgba(254, 253, 253, 0.1)" }}>
+              <CardContent>
+                <Typography variant="subtitle2" color="primary">
+                  Through my training in an accelerated bootcamp, I gained
+                  proficiency in Javascript and the PERN stack. I developed
+                  applications both solo and in teams, using Git and agile
+                  project management.
+                </Typography>
+              </CardContent>
+            </Card>
+          </motion.div>
         </TimelineContent>
       </TimelineItem>
     </Timeline>
